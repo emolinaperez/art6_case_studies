b4b09abf0a67715a4b43af880f92a74360b9e277
emolinaperez <edmundo.unam@gmail.com>
2025-03-11 23:59:01 -0600

    pero case study

---
+++ Peru_CaseStudy/code/data_prep_drivers.r
@@ -0,0 +1,93 @@
+#in mac  
+root<- "/Users/edmun/Library/CloudStorage/OneDrive-Personal/Edmundo-ITESM/3.Proyectos/51. WB Decarbonization Project/Peru_CaseStudy/"
+file.name <-"peru.csv"
+iso_code3 <- "PER"
+Country <- "peru"
+output.folder <- "/simulations raw/2025_03_10/"
+dir.data <- paste0(root,output.folder)
+
+#load data
+data <- read.csv(paste0(dir.data,file.name)) 
+data <- subset(data,region==Country)
+id_vars <-c('region','time_period',"primary_id")
+vars <- subset(colnames(data),!(colnames(data)%in%id_vars))
+library(data.table)
+data<-data.table::data.table(data)
+DT.m1 = melt(data, id.vars = id_vars,
+                   measure.vars = vars,
+             )
+DT.m1 <- data.frame(DT.m1)
+DT.m1$variable <- as.character(DT.m1$variable)
+sapply(DT.m1,class)
+
+#now read drivers taxonomy. 
+drivers <- read.csv(paste0(root,"simulations raw/driver_variables_taxonomy_20240117.csv"))
+
+#change column name to taxonomy 
+drivers$variable <- drivers$field
+drivers$field <- NULL 
+
+#merge
+ dim(DT.m1)
+ DT.m1 <- subset(DT.m1,variable%in%unique(drivers$variable))
+ dim(DT.m1)
+ 
+#
+#merge  
+ dim(DT.m1)
+ test2 <- merge(DT.m1,data.table(drivers),by="variable")
+ dim(test2)
+
+#
+#
+test2$Year <- test2$time_period + 2015 
+test2$time_period <- NULL 
+test2 <- subset (test2,Year>=2023)
+
+#read attribute primary
+att <- read.csv(paste0(root,output.folder,"ATTRIBUTE_PRIMARY.csv"))
+head(att)
+
+#merge 
+dim(test2)
+test2 <- merge(test2,att,by="primary_id")
+dim(test2)
+
+
+#merge stratgy atts 
+atts <- read.csv(paste0(root,output.folder,"ATTRIBUTE_STRATEGY.csv"))
+head(atts)
+#merge 
+dim(test2)
+test2 <- merge(test2,atts[c("strategy_id","strategy")],by="strategy_id")
+dim(test2)
+
+test2$Units <- "NA"
+test2$Data_Type <- "sisepuede simulation"
+test2$iso_code3<-iso_code3
+test2$Country <- Country
+test2$region <- NULL
+test2$subsector_total_field <- NULL
+test2$gas <- NA  
+
+test2$model_variable_information <- NULL
+test2$output_type<- "drivers"
+
+#create an additional sector variable for energy  
+energy_vars <- data.frame(variable=subset(unique(test2$variable),grepl("energy",unique(test2$variable))==TRUE ))
+energy_vars$energy_subsector <-"TBD"
+energy_vars$energy_subsector <- ifelse(grepl("ccsq",energy_vars$variable)==TRUE,"Carbon Capture and Sequestration",energy_vars$energy_subsector )
+energy_vars$energy_subsector <- ifelse(grepl("inen",energy_vars$variable)==TRUE,"Industrial Energy",energy_vars$energy_subsector )
+energy_vars$energy_subsector <- ifelse(grepl("entc",energy_vars$variable)==TRUE,"Power(electricity/heat)",energy_vars$energy_subsector )
+energy_vars$energy_subsector <- ifelse(grepl("trns",energy_vars$variable)==TRUE,"Transportation",energy_vars$energy_subsector )
+energy_vars$energy_subsector <- ifelse(grepl("scoe",energy_vars$variable)==TRUE,"Buildings",energy_vars$energy_subsector )
+
+#merge energy vars with test2 
+dim(test2)
+test2 <- merge(test2,energy_vars,by="variable", all.x=TRUE)
+dim(test2)
+
+test2 <- test2[order(test2$strategy_id,test2$model_variable,test2$subsector,test2$category_value,test2$Year),]
+
+write.csv(test2,paste0(root,"Tableau/drivers.csv"), row.names=FALSE)
+
---
+++ Peru_CaseStudy/code/run_script_baseline_run_new.r
@@ -0,0 +1,65 @@
+library(data.table)
+library(data.table)
+#regions 
+
+#Set root directory
+# in mac 
+root <- "/Users/edmun/Library/CloudStorage/OneDrive-Personal/Edmundo-ITESM/3.Proyectos/51. WB Decarbonization Project/Peru_CaseStudy/"
+
+#load emissions targets 
+#te_all<-read.csv(paste0(root,"/code/emission_targets.csv"))
+te_all<-read.csv(paste0(root,"/code/emission_targets_art6.csv"))
+target_country <- "PER"
+te_all<-te_all[,c("Subsector","Gas","Vars","Edgar_Class",target_country)]
+te_all[,"tvalue"] <- te_all[,target_country]
+te_all[,target_country] <- NULL
+target_vars <- unlist(strsplit(te_all$Vars,":"))
+
+#ouputfile
+output.folder <- "/simulations raw/2025_03_10/"
+output.file<-"sisepuede_results_sisepuede_run_2025-03-07T18;46;21.147694_WIDE_INPUTS_OUTPUTS.csv"
+
+#output.folder <- "/simulations raw/2025_02_11/"
+#output.file<-"sisepuede_results_sisepuede_run_2025-02-07T20;48;41.380587_WIDE_INPUTS_OUTPUTS.csv"
+
+
+
+data_all<-read.csv(paste0(root,output.folder,output.file))
+rall <- unique(data_all$region)
+
+#set params of rescaling function
+dir.output <- paste0(root,output.folder)
+initial_conditions_id <- "_0"
+time_period_ref <- 7
+
+dim(data_all)
+data_all <- subset(data_all,time_period>=time_period_ref)
+dim(data_all)
+
+#revise which sector-gas ids are zero at baseline 
+te_all$simulation <- 0
+for (i in 1:nrow(te_all))
+ {
+   # i<- 12
+    vars <- unlist(strsplit(te_all$Vars[i],":"))
+    if (length(vars)>1) {
+    te_all$simulation[i] <- as.numeric(rowSums(data_all[data_all$primary_id==gsub("_","",initial_conditions_id) &  data_all$time_period==time_period_ref,vars]))
+    } else {
+     te_all$simulation[i] <- as.numeric(data_all[data_all$primary_id==gsub("_","",initial_conditions_id) &  data_all$time_period==time_period_ref,vars])   
+    }
+  }
+te_all$simulation <- ifelse(te_all$simulation==0 & te_all$tvalue>0,0,1)
+correct<- aggregate(list(factor_correction=te_all$simulation),list(Edgar_Class=te_all$Edgar_Class),mean)
+te_all <- merge(te_all,correct,by="Edgar_Class")
+te_all$tvalue <- te_all$tvalue/te_all$factor_correction
+te_all$simulation<-NULL 
+te_all$factor_correction<-NULL
+te_all$Edgar_Class<-NULL
+
+#now run
+
+source(paste0(root,"/code/","rescale_function_baseline_mapping_timeref.r"))
+z<-1
+rescale(z,rall,data_all,te_all,initial_conditions_id,dir.output,time_period_ref)    
+
+
